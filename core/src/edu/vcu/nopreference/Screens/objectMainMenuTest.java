af4d1fdabe1048b6fb5ed6d933bba2e45670701e
AaronxRegen <43298788+AaronxRegen@users.noreply.github.com>
2018-10-17 19:24:58 -0400

    mainMenu is shown test

---
+++ core/src/edu/vcu/nopreference/Screens/objectMainMenuTest.java
@@ -0,0 +1,29 @@
+package edu.vcu.nopreference.Screens;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+//import edu.vcu.nopreference.Objects.base.GameTest;
+import edu.vcu.nopreference.Untitled;
+
+import static org.junit.Assert.*;
+
+public class objectMainMenuTest /*extends GameTest*/ {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMainMenu(){
+        mainMenu mockMenu = Mockito.mock(mainMenu.class);
+        assertFalse(mockMenu.isShown());
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
