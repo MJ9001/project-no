291256ef10a6ac38f269608777487a83be3ac9ce
AaronxRegen <43298788+AaronxRegen@users.noreply.github.com>
2018-10-17 20:52:06 -0400

    testMainMenuStart to test whether the game has been started

--- core/src/edu/vcu/nopreference/Screens/objectMainMenuTest.java
+++ core/src/edu/vcu/nopreference/Screens/objectMainMenuTest.java
@@ -24,18 +24,20 @@ public class objectMainMenuTest /*extends GameTest*/ {
         mainMenu mockMenu = Mockito.mock(mainMenu.class);
         assertFalse(mockMenu.isShown());
     }
+
     @Test
     public void testMainMenuStart(){//tests to see if when the start button is clicked on the main menu then the menu is hidden and the game is displayed
         mainMenu mockMenu = Mockito.mock(mainMenu.class);
         mockMenu.fakePress();
         assertFalse(mockMenu.isShown());
     }
+
     @Test
     public void testQuitToMenu(){//tests to see if main menu is being shown currently
-        objectMenu mockObjectMenu = Mockito.mock(objectMenu.class);
-        mockObjectMenu.fakeOnPress();
-        assertEquals(mockObjectMenu.returnToMain(), true);
+        objectMenu.fakeOnPress();
+        assertTrue(objectMenu.returnToMain());
     }
+
     @org.junit.After
     public void tearDown() throws Exception {
     }
