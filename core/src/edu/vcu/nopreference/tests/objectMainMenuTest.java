ceb60c7c22b7a9b5f8d4a371ae2e42b9e982637b
AaronxRegen <43298788+AaronxRegen@users.noreply.github.com>
2018-10-18 00:12:30 -0400

    Polished Comments

--- core/src/edu/vcu/nopreference/tests/objectMainMenuTest.java
+++ core/src/edu/vcu/nopreference/tests/objectMainMenuTest.java
@@ -11,12 +11,15 @@ import edu.vcu.nopreference.Screens.mainMenu;
 import static org.junit.Assert.*;
 
 public class objectMainMenuTest {
+
+    //Given that I am opening the game, when the game starts, then I should see a menu with an option to start the game
     @Test
     public void testMainMenu(){ //tests to see if main menu is being shown currently
         mainMenu mockMenu = Mockito.mock(mainMenu.class);
         assertFalse(mockMenu.isShown());
     }
 
+    //Given that I am in the main menu, when I press the start game button, then the game should start.
     @Test
     public void testMainMenuStart(){//tests to see if when the start button is clicked on the main menu then the menu is hidden and the game is displayed
         mainMenu mockMenu = Mockito.mock(mainMenu.class);
@@ -24,6 +27,7 @@ public class objectMainMenuTest {
         assertFalse(mockMenu.isShown());
     }
 
+    //Given that I am playing the game, when I press quit, then the main menu should appear
     @Test
     public void testQuitToMenu(){//tests to go back to main menu from pause menu
         inGame.paused = true;
