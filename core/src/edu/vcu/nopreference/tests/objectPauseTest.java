5ca525e4a001425931c9744005f868a98cd920d8
AaronxRegen <43298788+AaronxRegen@users.noreply.github.com>
2018-11-11 00:46:37 -0500

    testQuitToMenu modified

--- core/src/edu/vcu/nopreference/tests/objectPauseTest.java
+++ core/src/edu/vcu/nopreference/tests/objectPauseTest.java
@@ -1,29 +1,16 @@
 package edu.vcu.nopreference.tests;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.graphics.GL20;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import edu.vcu.nopreference.Objects.base.inputHandler;
-import edu.vcu.nopreference.Objects.base.objectBase;
+import edu.vcu.nopreference.Objects.base.Platform;
 import edu.vcu.nopreference.Objects.base.objectManager;
-import edu.vcu.nopreference.Objects.base.objectMenu;
 import edu.vcu.nopreference.Objects.base.objectPause;
 import edu.vcu.nopreference.Objects.renders.Render;
 import edu.vcu.nopreference.Screens.inGame;
-import edu.vcu.nopreference.Screens.mainMenu;
 import edu.vcu.nopreference.Untitled;
 
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 public class objectPauseTest{
 
@@ -79,37 +66,16 @@ public class objectPauseTest{
     public void testPauseQuit(){
         Render.testing = true;
         objectManager OMTest = new objectManager();
-        OMTest.initializeObjects();
-        OMTest.logicTick();
+
+        Platform tempPlat = new Platform();
+        tempPlat.posX = 500;
+
         objectPause fakePause = new objectPause();
-
         OMTest.onClick((int)(50),50);//Click topleft corner.
+
+        OMTest.logicTick();
         OMTest.logicTick();
 
-        OMTest.onClick(Render.getWidth()/2, Render.getHeight()/2);//Click topleft corner.
-        assertTrue(Untitled.newScreen instanceof mainMenu);
+        assertEquals(500, tempPlat.posX,1);
     }
-
-    /*@org.junit.After
-    public void tearDown() throws Exception {
-    }
-
-    private static Application application;
-
-    @BeforeClass
-    public static void init() {
-        // Note that we don't need to implement any of the listener's methods
-        application = new HeadlessApplication(new ApplicationListener() {
-            @Override public void create() {}
-            @Override public void resize(int width, int height) {}
-            @Override public void render() {}
-            @Override public void pause() {}
-            @Override public void resume() {}
-            @Override public void dispose() {}
-        });
-
-        // Use Mockito to mock the OpenGL methods since we are running headlessly
-        Gdx.gl20 = Mockito.mock(GL20.class);
-        Gdx.gl = Gdx.gl20;
-    }*/
 }
